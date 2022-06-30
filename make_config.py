fd0452bae9efbbe6a792f8645c7115a225d58025
trwiley <taylor.r.wiley@gmail.com>
2022-06-30 10:48:29 -0400

    moved project config over to the script itself in order to be able to use the generated project ID.

--- make_config.py
+++ make_config.py
@@ -9,8 +9,15 @@ config_input = json.loads(config_data)
 
 config = configparser.ConfigParser()
 
+config['project'] = {"cpu": 5,
+        "projname": "taylor_nanopore_barcode2_run",
+        "projid": 8,
+        "projcode": sys.argv[1],
+        "projowner": "twiley1@nd.edu",
+        "projrunhost": "http://localhost"}
+
 for key in config_input:
     config[key] = config_input[key]
-    
-with open('config.txt', 'w') as configfile:
+
+with open(f'{sys.argv[1]}/config.txt', 'w') as configfile:
     config.write(configfile)
